340858842891945ae5683fec3436e0128ac05509
Gurudatta <gurudatta@Gurudattas-MacBook-Air.local>
2018-09-30 12:37:50 +0530

    Delete the unused urls

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import CreateView
-
-urlpatterns = [
-    path('dataset/', CreateView.as_view(), name='create'),
-]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Dataset1
 from .serializers import Dataset1Serializer
 from django.http import Http404
 from rest_framework.views import APIView
@@ -11,10 +10,6 @@ from django.http import JsonResponse
 from django.core import serializers
 from django.conf import settings
 import json
-
-class CreateView(generics.ListCreateAPIView):
-    queryset = Dataset1
-    serializer_class = Dataset1Serializer
 
 @api_view(["POST"])
 def Greetings(name):
